9ce4f883670b9b0be9be98dc13723f2bf4176b00
tunglam1302 <47854205+tunglam1302@users.noreply.github.com>
2019-05-25 17:31:05 +0700

    Add files via upload

---
+++ src/AS1/Fibonaci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_demo.AS1
+{
+    class Fibonaci
+    {
+        public void fibonaci()
+        {
+            int n;
+            int f1 = 0, f2 = 1, f3;
+            int sum = 1;
+            Console.WriteLine("Nhap n= ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Tinh tong " + n + " so hang dau tien cua day Fibonacci");
+            for (int i = 2; i < n; i++)
+            {
+                f3 = f1 + f2;
+                sum = sum + f3;
+                f1 = f2;
+                f2 = f3;
+            }
+            Console.WriteLine("" + sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/AS1/Songuyento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_demo.AS1
+{
+    class Songuyento
+    {
+        public void songuyento()
+        {
+            int n, number;
+            Console.WriteLine("Tim cac so nguyen to nho hon n\n");
+            Console.WriteLine("Nhap vao so n");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (number = 1; number <= n; number++)
+            {
+                int dem = 0;
+                for (int i = 1; i <= number / 2; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        dem++;
+                    }
+                }
+                if (dem == 1)
+                {
+                    Console.WriteLine(+number + " la so nguyen to");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
